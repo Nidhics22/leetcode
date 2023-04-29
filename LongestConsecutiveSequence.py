e40fdb10da3c4d9fbe8ce98d9704de4d5d0c5af2
Nidhi Prajapati <nidhiprajapati@Nidhis-Air.attlocal.net>
2023-04-29 12:48:52 -0700

    added leetcode 128

---
+++ LongestConsecutiveSequence.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        LEETCODE #128
+        """
+        if (len(nums) == 0):
+            return 0
+        nums = sorted(nums)
+        i = 0
+        j = i + 1
+        currentCounter = 1
+        maxCounter = 0
+        while (j < len(nums)):
+            val = nums[j] - nums[i]
+            i += 1
+            j += 1
+            if (val == 1):
+                currentCounter += 1
+            elif (val == 0):
+                continue
+            else:
+                maxCounter = max(currentCounter, maxCounter)
+                currentCounter = 1
+        return (max(currentCounter, maxCounter))
+
+
