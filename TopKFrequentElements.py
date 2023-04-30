57782e816f8467db1994619572ed36b7ef81c425
Nidhi Prajapati <nidhiprajapati@Nidhis-MacBook-Air.local>
2023-04-30 16:26:50 -0700

    added TopKFrequentElements

---
+++ TopKFrequentElements.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for val in freq[i]:
+                res.append(val)
+                if (len(res) == k):
+                    return res
